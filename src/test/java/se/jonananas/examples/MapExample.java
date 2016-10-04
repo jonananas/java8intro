0248f5f01f5ef810f8a37337413fe35d8b6bf229
Jonas Andersson <jonas.a.andersson@umu.se>
2016-10-04 15:50:22 +0200

    Added MapExample

---
+++ src/test/java/se/jonananas/examples/MapExample.java
@@ -0,0 +1,75 @@
+package se.jonananas.examples;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import org.junit.Test;
+
+public class MapExample {
+
+	List<String> namnlista = Arrays.asList("Ellen", "Mikael", "Eyadh", "Jonas");
+
+	@Test
+	public void shouldFilterNamesWithMoreThan10Chars() {
+		Predicate<String> mindreÄnTio = namn -> namn.length() < 10;
+		Predicate<String> predicate = (Predicate<String>) namn -> namn.length() > 5;
+		List<String> resultat = Stream.of("Ellen", "Mikael", "Eyadh", "Jonasas") //
+				.filter(predicate.and(mindreÄnTio))
+				.collect(toList());
+
+		assertThat(resultat).containsExactly("Mikael", "Jonasas");
+	}
+
+	@Test
+	public void groupByLength() {
+		Map<Integer, List<String>> expectedByLength = new HashMap<>();
+		expectedByLength.put(5, Arrays.asList("Ellen", "Eyadh", "Jonas"));
+		expectedByLength.put(6, Arrays.asList("Mikael"));
+
+		Map<Integer, List<String>> resultMap = namnlista.stream() //
+				.collect(groupingBy(x -> x.length()));
+
+		assertThat(resultMap).isEqualTo(expectedByLength);
+	}
+
+	@Test
+	public void imperative() {
+		Map<Integer, List<String>> expectedByLength = new HashMap<>();
+		expectedByLength.put(5, Arrays.asList("Ellen", "Eyadh", "Jonas"));
+		expectedByLength.put(6, Arrays.asList("Mikael"));
+
+		Map<Integer, List<String>> resultMap = new HashMap<>();
+		for (String namn : namnlista) {
+			if (resultMap.get(namn.length()) == null) {
+				resultMap.put(namn.length(), new ArrayList<String>());
+			}
+			resultMap.get(namn.length()).add(namn);
+		}
+
+		assertThat(resultMap).isEqualTo(expectedByLength);
+	}
+	// Övning: Gruppera på första bokstav i namnet
+
+	@Test
+	public void groupByFirstLetter() {
+		Map<String, List<String>> expected = new HashMap<>();
+		expected.put("E", Arrays.asList("Ellen", "Eyadh"));
+		expected.put("M", Arrays.asList("Mikael"));
+		expected.put("J", Arrays.asList("Jonas"));
+
+		Map<String, List<String>> resultMap = namnlista.stream() //
+				.collect(groupingBy(x -> x.substring(0, 1)));
+
+		assertThat(resultMap).isEqualTo(expected);
+	}
+
+}
